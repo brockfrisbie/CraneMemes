d6f61155391fe0ace87c86f59072861aaf96788e
Brock Frisbie <brockfrisbie@gmail.com>
2014-01-16 17:54:11 -0500

    Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraneMemes::Application.config.secret_key_base = '7acbe2b538c87d75143c9cb35447de3a7c22be4fcbb4ca61a55dc869e0c1197095cefd2defe509c0a79f1123816e38ccd100f6d2e0965d528bd552f36f6a11bd'
