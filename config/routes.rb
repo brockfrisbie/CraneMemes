d3c434e26f4be1229ae7dedb009be1c4dce628f0
Brock Frisbie <brockfrisbie@gmail.com>
2014-01-18 14:10:41 -0500

    Updated Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CraneMemes::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "submit" => "pages#submit"
+  get "store" => "pages#store"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
